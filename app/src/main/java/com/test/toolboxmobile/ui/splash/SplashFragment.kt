9b415b38e0f553dbe2b1753caa3db64a0d8a70fb
bartmejia18 <bart.1890mr@gmail.com>
2021-09-23 10:37:16 -0600

    theme colors changed

--- app/src/main/java/com/test/toolboxmobile/ui/splash/SplashFragment.kt
+++ app/src/main/java/com/test/toolboxmobile/ui/splash/SplashFragment.kt
@@ -1,8 +1,16 @@
 package com.test.toolboxmobile.ui.splash
 
+import android.os.Bundle
+import android.view.View
+import androidx.appcompat.app.AppCompatActivity
 import androidx.fragment.app.Fragment
 import com.test.toolboxmobile.R
 
 class SplashFragment: Fragment(R.layout.fragment_splash) {
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
 
+        (activity as AppCompatActivity?)!!.supportActionBar!!.hide()
+
+    }
 }
