e5a28e00f63dd76086494a2ffd86c3d1303c1a9d
bartmejia18 <bart.1890mr@gmail.com>
2021-09-23 10:13:03 -0600

    fragment splash added

--- app/src/main/java/com/test/toolboxmobile/MainActivity.kt
+++ app/src/main/java/com/test/toolboxmobile/MainActivity.kt
@@ -1,4 +1,4 @@
-package com.universales.toolboxmobile
+package com.test.toolboxmobile
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
---
+++ app/src/main/java/com/test/toolboxmobile/ui/splash/SplashFragment.kt
@@ -0,0 +1,8 @@
+package com.test.toolboxmobile.ui.splash
+
+import androidx.fragment.app.Fragment
+import com.test.toolboxmobile.R
+
+class SplashFragment: Fragment(R.layout.fragment_splash) {
+
+}
